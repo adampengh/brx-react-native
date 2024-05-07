1e532232c3006537390d0242d1ebc634ec43d44a
Adam Pengh <adam.pengh@bloomreach.com>
2024-05-07 10:25:54 -0400

    add README

---
+++ README.md
@@ -0,0 +1,23 @@
+[![runs with Expo Go](https://img.shields.io/badge/Runs%20with%20Expo%20Go-4630EB.svg?style=flat-square&logo=EXPO&labelColor=f3f3f3&logoColor=000)](https://expo.dev/client)
+
+# BrX React Native
+
+
+## Installation
+
+```
+yarn
+```
+or
+```
+npm ci
+```
+
+## Configuration
+Rename the `.env.sample` file to `.env` and set the `EXPO_PUBLIC_BRXM_ENDPOINT` environment variable in the `.env` file to your environment's endpoint
+
+## Run the Project
+
+```
+yarn start
+```
--- src/lib/bloomreach/configuration.ts
+++ src/lib/bloomreach/configuration.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 import { Configuration } from '@bloomreach/spa-sdk';
 import { Platform } from 'react-native';
 
-const PREVIEW_TOKEN_KEY: string = 'token';
-const PREVIEW_SERVER_ID_KEY: string = 'server-id';
+const PREVIEW_MULTI_TENANT_ENDPOINT_PARAM: string = 'endpoint';
+const PREVIEW_TOKEN_PARAM: string = 'token';
+const PREVIEW_SERVER_ID_KEY_PARAM: string = 'server-id';
 
 const getBrxConfiguration = (path: string) => {
   const configuration: Configuration = {
@@ -13,17 +14,22 @@ const getBrxConfiguration = (path: string) => {
     debug: true,
   };
 
-  // For preview mode in Experience Manager
+  // For Preview mode in Experience Manager
   if (Platform.OS === 'web') {
     // @ts-ignore
     const urlParams = new URLSearchParams(window.location.search);
 
-    const authorizationToken = urlParams.get(PREVIEW_TOKEN_KEY);
+    const endpoint = urlParams.get(PREVIEW_MULTI_TENANT_ENDPOINT_PARAM);
+    if (endpoint) {
+      configuration.endpoint = endpoint;
+    }
+
+    const authorizationToken = urlParams.get(PREVIEW_TOKEN_PARAM);
     if (authorizationToken) {
       configuration.authorizationToken = authorizationToken;
     }
 
-    const serverId = urlParams.get(PREVIEW_SERVER_ID_KEY);
+    const serverId = urlParams.get(PREVIEW_SERVER_ID_KEY_PARAM);
     if (serverId) {
       configuration.serverId = serverId;
     }
